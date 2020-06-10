8edb5587b71f688cdb5db67f55596ee6f690df63
lawrencecchen <md.lawrence.chen@gmail.com>
2020-06-10 16:23:33 -0700

    improve SEO

--- next.config.js
+++ next.config.js
@@ -1,2 +1,10 @@
 const withImages = require('next-images');
+const sitemap = require('nextjs-sitemap-generator');
+
 module.exports = withImages();
+
+sitemap({
+  baseUrl: 'embracethecurve.org',
+  pagesDirectory: __dirname + '/pages',
+  targetDirectory: 'public/',
+});
